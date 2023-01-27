4a5d2d74748f784deab99415f7e2bb1c4e4dc554
brucew5978-git <83440706+brucew5978-git@users.noreply.github.com>
2023-01-27 15:41:49 -0500

    Delete audio_playing.py

--- audio_playing.py
+++
@@ -1,8 +0,0 @@
-import sounddevice
-import soundfile
-
-filename = "output_waveglow.wav"
-
-data, sampleRate = soundfile.read(filename, dtype='float32')
-sounddevice.play(data, sampleRate)
-status = sounddevice.wait()
